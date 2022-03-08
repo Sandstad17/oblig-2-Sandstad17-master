5ebe172e1c856c20eaaa8c9fba62ca4acd7e3419
Sandstad17 <90138783+Sandstad17@users.noreply.github.com>
2022-03-08 13:13:16 +0100

    fullstendig nytt projekt

---
+++ src/test/java/com/example/oblig2boot/Oblig2bootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig2boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2bootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
